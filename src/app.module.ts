4641aa87b4c46f4a57e3ef733f10b4c82b4402c3
Traore Zie <zie.traore@etudiant.univ-rennes.fr>
2026-02-27 17:00:11 +0100

    conexion typeORM avec la BD

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,30 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import databaseConfig from './config/database.config';
+import { TypeOrmModule, TypeOrmModuleOptions } from '@nestjs/typeorm';
 
 @Module({
-  imports: [],
+
+  imports: [
+    // Configuration des variables d'environnement
+    ConfigModule.forRoot({
+      isGlobal : true,
+      envFilePath : '.env',
+      load : [ databaseConfig ],
+    }),
+    // Configuration de TypeORM avec les options chargées depuis ConfigService
+    TypeOrmModule.forRootAsync({
+      imports : [ConfigModule],
+      inject : [ConfigService],
+      useFactory : (config : ConfigService) : TypeOrmModuleOptions =>
+        config.get<TypeOrmModuleOptions>('database')!,
+    })
+  ],
+
   controllers: [AppController],
+
   providers: [AppService],
 })
 export class AppModule {}
--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,19 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+
+  // Préfix global pour toutes les routes de l'API
+  app.setGlobalPrefix('lemobici-api/v1');
+
+  // Configuration CORS pour autoriser le frontend à accéder à l'API
+  app.enableCors({
+    origin: process.env.FRONTEND_URL ?? 'http://localhost:4200', // Angular dev
+    credentials: true,
+  });
+
+  // Demarrer l'API
+  const port = process.env.API_PORT ?? 3000;
+  await app.listen(port);
+  console.log(`LeMobici API démarrée sur http://localhost:${port}/lemobici-api/v1 •••`)
 }
 bootstrap();
